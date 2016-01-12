fc2859918c0d55339d64b3d09091eb8879459a77
Shruti Arora <shruti.arora89@icloud.com>
2016-01-12 14:24:09 -0800

    adding index.html,game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+
+
+var question1 = prompt('Did I grow up in USA?');
+console.log('The answer is'+ question1);
+alert('Your answer is '+ question1);
+
+var question2 = prompt('Have I been in US for more than 2 years');
+console.log('The answer is'+ question2);
+alert('Your answer is '+ question2);
+
+var question3 = prompt('Do I like the Raining Seattle Weather?');
+console.log('The answer is'+ question3);
+alert('Your answer is '+ question3);
