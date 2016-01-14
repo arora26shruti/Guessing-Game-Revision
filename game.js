1178eb01565da391e39c2eb0a558d00cdbf1bc8a
Shruti Arora <shruti.arora89@icloud.com>
2016-01-13 22:01:11 -0800

    adding if/else statement

--- game.js
+++ game.js
@@ -1,19 +1,39 @@
 
 
-var question1 = prompt('Did I grow up in USA?');
-console.log('The answer is'+ question1);
-alert('Your answer is '+ question1);
+var userName = prompt('What is your Name?');
+alert('Its Nice to meet you, '+userName+' ,Great Name by the way!');
 
-var question2 = prompt('Have I been in US for more than 2 years');
-console.log('The answer is'+ question2);
-alert('Your answer is '+ question2);
+var count=0;
+//////////////////////////////////////////////////////////////////////////////////////////
+var answer1 = prompt('Did Shruti grew up in USA?');
 
-var question3 = prompt('Do I like the Raining Seattle Weather?');
-console.log('The answer is'+ question3);
-alert('Your answer is '+ question3);
-
-
-
+if(answer1=='Y' ||answer1=='YES'||answer1=='yes' || answer1== 'y'){
+	alert('No, sorry '+ userName +', Shruti was not born in U.S.A!');
+}
+else if(answer1=='N'||answer1=='NO'||answer1=='no'||answer1=='n'){
+	alert('Yay! You got it right, '+ userName +' , Shruti was born in India, not U.S.A.');
+	count++;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+var answer2 = prompt('Has Shruti been in US for more than 2 years');
+if(answer2=='Y'||answer2 =='YES'||answer2 =='yes'||answer2=='y'){
+	alert('No, sorry '+ userName + ' ,She has been here only for last nine months');
+}
+else if(answer2=='N'||answer2=='NO'||answer2=='no'||answer2=='n'){
+	alert('Yay! You got it right, '+ userName +' , Shruti just came here few months ago.');
+	count++;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+var answer3 = prompt("Does Shruti like the Seattle\'s Raining Weather?");
+if(answer3 =='Y'||answer3 =='YES'||answer3 =='yes'||answer3 =='y'){
+	alert("No, sorry "+ userName +" Shruti doesn\'t like the raining throughout the year");
+}
+else if(answer3 =='N'||answer3 =='NO' ||answer3 =='no' ||answer3 =='n'){
+	alert("Yay! You got it right, "+ userName +" , Shruti doesn\'t like the weather.");
+	count++;
+}
+	alert('You got '+ count+' out of 3 correct answers, '+ userName+' ,Thanks for playing!');
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 
